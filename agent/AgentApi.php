56ad71bbbd338bc71fe6e1ae03d2446748ff69d4
sin <sin@megalive.com.tw>
2017-08-23 17:40:21 +0800

    [add] 範例 實作 兩種案例

--- agent/AgentApi.php
+++ agent/AgentApi.php
@@ -51,4 +51,20 @@ class AgentApi
     {
         $this->apiKey = $apiKey;
     }
+
+    /**
+     * @param $url
+     * @return mixed
+     */
+    public function goCurl($url)
+    {
+        // create curl resource
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        return $output;
+    }
 }
--- agent/AgentGetKeySample.php
+++ agent/AgentGetKeySample.php
@@ -1,13 +1,30 @@
 <?php
 require_once('AgentApi.php');
+$apiHost = 'http://api.oh-gaming.com/api/';
+
+$apiKey = ''; //you api_key
+$memberId = ''; //you account
 
 //get api key
-$apiKey = 'key';
-$memberId = 'memberId';
 $apiKeyTool = new AgentApi($apiKey, $memberId);
-$youApiKey = $apiKeyTool -> getKey();
+$youApiKey = $apiKeyTool->getKey();
 
 //get new api key
-$apiKeyTool -> setApiKey('you new key');
-$apiKeyTool -> setMemberId('you new memberId');
-$youApiKey = $apiKeyTool -> getKey();
+$apiKeyTool->setApiKey('you new key');
+$apiKeyTool->setMemberId('you new memberId');
+$youNewApiKey = $apiKeyTool->getKey();
+
+
+//Example 1 get lang list
+//this api url : http://<api-server>/api/lang_list/{Key}
+
+$exampleOneUrl = $apiHost . 'lang_list/' . $youApiKey;
+$responseOne = $apiKeyTool->goCurl($exampleOneUrl);
+
+
+//Example 2 get user info
+//this api url : http://<api-server>/api/user_info/{Key}/{User}
+
+$user = ''; //you member account
+$exampleTwoUrl = $apiHost . 'user_info/' . $youApiKey . '/' . $user;
+$responseTwo = $apiKeyTool->goCurl($exampleTwoUrl);
